50e6084c5ab5302906e240b695fda7befde32cd5
Ally <allyson.lifter@gmail.com>
2020-08-17 19:15:13 -0400

    fixing subfolder links

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,28 @@
 $(function () {
-    $("#header-placeholder").load("../header.html");
+    let year = new Date();
 
-    $("#nav-placeholder").load("../nav.html", function () {
+    $("#header-placeholder").load("/brown/header.html");
+
+    $("#nav-placeholder").load("/brown/nav.html", function () {
         let pageLoc = window.location.pathname;
-        let navLink = pageLoc.split('/');
+        let navLink = pageLoc.split("/");
         console.log(pageLoc);
-        console.log(navLink);
        
         if ((pageLoc == '') || (pageLoc == '/')) {
-            $('.home').addClass('active');
+            $('.home').addClass("active");
             $('.home').attr('href', '#');
         }
         else {
             let navShift = navLink.slice(1,2);
             console.log(navShift);
 
-            $('a[href="' + pageLoc + '"]').addClass('active');
+            $('a[href="' + pageLoc + '"]').addClass("active");
             $('a[href="' + pageLoc + '"]').attr('href', '#');
-            $('[class$="' + navShift + '"]').addClass('active');
+            $('[class$="' + navShift + '"]').addClass("active");
         }
     });
 
-    $("#footer-placeholder").load("../footer.html", function () {
-        let year = new Date();
+    $("#footer-placeholder").load("/brown/footer.html", function () {
         $("#year").text(year.getFullYear());
     });
 });
