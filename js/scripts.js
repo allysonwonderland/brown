96214a135c662c51f5faed85884745690a86ebb3
Ally <allyson.lifter@gmail.com>
2020-08-17 15:36:10 -0400

    navigation edits

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,17 @@
-<script>
-    $(function(){
-        $("#nav-placeholder").load("nav.html")
-    });
-</script>
+$(function() {    
+    $("#header-placeholder").load("header.html");
+
+    $("#nav-placeholder").load("nav.html", function () {
+        let pageLoc = location.pathname.split("/");
+        let navLink = pageLoc.pop();
+
+        $('a[href="' + navLink + '"]').addClass("active");
+        $('a[href="' + navLink + '"]').attr('href', '#');
+
+        // let site = "http://allysonwonderland.github.io/brown/index.html";
+        // let popSite = site.split("/");
+        // console.log(popSite.pop());    
+    });    
+    
+    $("#footer-placeholder").load("footer.html");
+});
