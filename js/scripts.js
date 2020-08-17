f54e564a3d4cafd6ab0731f778eb37babf6d5c8d
Ally <allyson.lifter@gmail.com>
2020-08-17 18:21:07 -0400

    more navigation fixes

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,28 @@
-$(function() {    
-    $("#header-placeholder").load("header.html");
+$(function () {
+    $("#header-placeholder").load("/header.html");
 
-    $("#nav-placeholder").load("nav.html", function () {
-        let pageLoc = location.pathname.split("/");
-        let navLink = pageLoc.pop();
+    $("#nav-placeholder").load("/nav.html", function () {
+        let pageLoc = window.location.pathname;
+        let navLink = pageLoc.split('/');
+        console.log(pageLoc);
+        console.log(navLink);
+       
+        if ((pageLoc == '') || (pageLoc == '/')) {
+            $('.home').addClass('active');
+            $('.home').attr('href', '#');
+        }
+        else {
+            let navShift = navLink.slice(1,2);
+            console.log(navShift);
 
-        $('a[href="' + navLink + '"]').addClass("active");
-        $('a[href="' + navLink + '"]').attr('href', '#');
+            $('a[href="' + pageLoc + '"]').addClass('active');
+            $('a[href="' + pageLoc + '"]').attr('href', '#');
+            $('[class$="' + navShift + '"]').addClass('active');
+        }
+    });
 
-        // let site = "http://allysonwonderland.github.io/brown/index.html";
-        // let popSite = site.split("/");
-        // console.log(popSite.pop());    
-    });    
-    
-    $("#footer-placeholder").load("footer.html");
+    $("#footer-placeholder").load("/footer.html", function () {
+        let year = new Date();
+        $("#year").text(year.getFullYear());
+    });
 });
