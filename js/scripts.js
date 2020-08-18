050c65902ce777dc43506f38fe5aceac63aa1200
Ally <allyson.lifter@gmail.com>
2020-08-18 08:24:23 -0400

    testing js imports

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,34 @@
-$(function () {
-    $.when(
-        $.getScript('https://code.jquery.com/jquery-3.5.1.js'),
-        $.getScript('https://code.jquery.com/jquery-3.5.1.min.js'),
-        $.getScript('https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js'),
-        $.getScript('https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js'),
-        $.getScript('https://kit.fontawesome.com/1c11d0daec.js'),
-        $.Deferred(function (deferred) {
-            $(deferred.resolve);
-        })
-    ).done(function () {
-        let year = new Date();
+$.when(
+    $.getScript('https://code.jquery.com/jquery-3.5.1.js'),
+    $.getScript('https://code.jquery.com/jquery-3.5.1.min.js'),
+    $.getScript('https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js'),
+    $.getScript('https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js'),
+    $.getScript('https://kit.fontawesome.com/1c11d0daec.js'),
+    $.Deferred(function (deferred) {
+        $(deferred.resolve);
+    })
+).done(function () {
+    let year = new Date();
 
-        $("#header-placeholder").load("/brown/header.html");
+    $("#header-placeholder").load("/brown/header.html");
 
-        $("#nav-placeholder").load("/brown/nav.html", function () {
-            let pageLoc = window.location.pathname;
-            console.log("Page location: " + pageLoc);
-            let navLink = pageLoc.split("/");
-            console.log("Page array: " + navLink);
-            let navPop = navLink.pop();
-            console.log("First popped item: " + navPop);
-            navPop = navLink.pop();
-            console.log("Final popped item: " + navPop);
+    $("#nav-placeholder").load("/brown/nav.html", function () {
+        let pageLoc = window.location.pathname;
+        console.log("Page location: " + pageLoc);
+        let navLink = pageLoc.split("/");
+        console.log("Page array: " + navLink);
+        let navPop = navLink.pop();
+        console.log("First popped item: " + navPop);
+        navPop = navLink.pop();
+        console.log("Final popped item: " + navPop);
 
-            $('a[href$="' + pageLoc + '"]').addClass("active");
-            $('a[href$="' + pageLoc + '"]').attr('href', '#');
-            $('[class$="' + navPop + '"]').addClass("active");
-        });
+        $('a[href$="' + pageLoc + '"]').addClass("active");
+        $('a[href$="' + pageLoc + '"]').attr('href', '#');
+        $('[class$="' + navPop + '"]').addClass("active");
+    });
 
-        $("#footer-placeholder").load("/brown/footer.html", function () {
-            $("#year").text(year.getFullYear());
-        });
+    $("#footer-placeholder").load("/brown/footer.html", function () {
+        $("#year").text(year.getFullYear());
     });
 });
+
