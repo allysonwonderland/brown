f0a24873bf4966b5f0f86db052212cf33d4c758b
Ally <allyson.lifter@gmail.com>
2020-08-18 07:24:46 -0400

    jQuery updates

--- js/scripts.js
+++ js/scripts.js
@@ -7,19 +7,15 @@ $(function () {
         let pageLoc = window.location.pathname;
         let navLink = pageLoc.split("/");
         let navPop = navLink.pop();
-        navPop = navLink.pop();
 
         $('a[href$="' + pageLoc + '"]').addClass("active");
         $('a[href$="' + pageLoc + '"]').attr('href', '#');
         $('[class$="' + navPop + '"]').addClass("active");
 
         console.log("Page location: " + pageLoc);
-        console.log("Page array: " + navLink);
-        console.log("Page array length: " + navLink.length);
-        // console.log("Popped last item off array: " + homeLink); 
-        console.log("Page array: " + navLink);
-        console.log("Popped last item off array 1: " + navPop); 
-        console.log("Popped last item off array 2: " + navPop);      
+        console.log(navLink);
+        console.log(navLink.length);
+        console.log("Popped last item off array: " + navPop);      
     });
 
     $("#footer-placeholder").load("/brown/footer.html", function () {
