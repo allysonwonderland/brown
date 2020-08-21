43497ed503f565b3ce1ab8f37d7b6b9179eb6bc8
Ally <allyson.lifter@gmail.com>
2020-08-21 16:11:21 -0400

    breadcrumbs in jquery

--- js/scripts.js
+++ js/scripts.js
@@ -5,14 +5,11 @@ $(function () {
         $.getScript('https://kit.fontawesome.com/1c11d0daec.js'),
     ).done(function () {
         let year = new Date();
+
         let pageLoc = window.location.pathname;
-        console.log("Page location: " + pageLoc);
         let navLink = pageLoc.split("/");
-        console.log("Page array: " + navLink);
         let pagePop = navLink.pop();
-        console.log("First popped item: " + pagePop);
         let navPop = navLink.pop();
-        console.log("Final popped item: " + navPop);
 
         let pageLink = ('a[href$="' + pageLoc + '"]');
         let pageNav = ('[class$="' + navPop + '"]');
@@ -44,21 +41,18 @@ $(function () {
         let breadcrumbPagePop = pagePop.replace('.html', '');
         breadcrumbPagePop = breadcrumbPagePop.substr(0, 1).toUpperCase() + breadcrumbPagePop.substr(1);
         let breadcrumbNav = navPop.substr(0, 1).toUpperCase() + navPop.substr(1);
-        console.log(pagePop);
 
         if ($("ol").has("#breadcrumb")) {
             console.log(pageLoc);
-            if (!(pagePop.trim())) {
+            if (!(pagePop.trim()) || (pagePop.indexOf("index") >= 0) ) {
                 console.log("parent test");
                 $("#breadcrumb").html('<li class="breadcrumb-item"><a href="/brown/index.html">Home</a></li><li class="breadcrumb-item active">' + breadcrumbNav + '</li>');
-            }     
-            else  {
-            // ($(pagePop).has('.html') !== -1) {
+            }
+            else {
+                // ($(pagePop).has('.html') !== -1) {
                 console.log("child test");
                 $("#breadcrumb").html('<li class="breadcrumb-item"><a href="/brown/index.html">Home</a></li><li class="breadcrumb-item"><a href="./">' + breadcrumbNav + '</a></li><li class="breadcrumb-item active">' + breadcrumbPagePop + '</li>');
             }
-       
         }
     });
-
 });
