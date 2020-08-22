a493702faf7bf95249c6af5051a6d8998f068b4b
Ally <allyson.lifter@gmail.com>
2020-08-21 20:37:01 -0400

    more jquery updates

--- js/scripts.js
+++ js/scripts.js
@@ -40,19 +40,26 @@ $(function () {
         });
 
         let breadcrumbPagePop = pagePop.replace('.html', '');
+        let breadcrumbPage = '';
 
         if (breadcrumbPagePop.indexOf("-") >= 0) {
             if (breadcrumbPagePop.indexOf("usgenweb") >= 0) {
                 breadcrumbPagePop = breadcrumbPagePop.split('-');
                 breadcrumbPagePop = breadcrumbPagePop[0].substr(0, 3).toUpperCase() +  breadcrumbPagePop[0].substr(3,2) + breadcrumbPagePop[0].substr(5,1).toUpperCase() + breadcrumbPagePop[0].substr(6) + " " + breadcrumbPagePop[1].substr(0, 1).toUpperCase() + breadcrumbPagePop[1].substr(1);
+                
+                breadcrumbPage = breadcrumbPagePop;
             }
             else {
                 breadcrumbPagePop = breadcrumbPagePop.split('-');
-                breadcrumbPagePop = breadcrumbPagePop[0].substr(0, 1).toUpperCase() + breadcrumbPagePop[0].substr(1) + " " + breadcrumbPagePop[1].substr(0, 1).toUpperCase() + breadcrumbPagePop[1].substr(1);
+                console.log(breadcrumbPagePop.length);
+                for (let i = 0; i < breadcrumbPagePop.length; i++) {
+                    breadcrumbPage += breadcrumbPagePop[i].substr(0, 1).toUpperCase() + breadcrumbPagePop[i].substr(1) + " ";
+                    console.log(breadcrumbPage);
+                }
             }
         }
         else {
-            breadcrumbPagePop = breadcrumbPagePop.substr(0, 1).toUpperCase() + breadcrumbPagePop.substr(1);
+            breadcrumbPage = breadcrumbPagePop.substr(0, 1).toUpperCase() + breadcrumbPagePop.substr(1);
         }
         
         let breadcrumbNav = navPop.substr(0, 1).toUpperCase() + navPop.substr(1);
@@ -66,8 +73,8 @@ $(function () {
             else {
                 // ($(pagePop).has('.html') !== -1) {
                 console.log("child test");
-                $("#breadcrumb").html('<li class="breadcrumb-item"><a href="/brown/index.html">Home</a></li><li class="breadcrumb-item"><a href="index.html">' + breadcrumbNav + '</a></li><li class="breadcrumb-item active">' + breadcrumbPagePop + '</li>');
+                $("#breadcrumb").html('<li class="breadcrumb-item"><a href="/brown/index.html">Home</a></li><li class="breadcrumb-item"><a href="index.html">' + breadcrumbNav + '</a></li><li class="breadcrumb-item active">' + breadcrumbPage + '</li>');
             }
-        }
+        }        
     });
 });
