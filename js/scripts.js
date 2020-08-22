fbf5df6df5f1c4187b4f3cdc0d7bd5a1e538743d
Ally <allyson.lifter@gmail.com>
2020-08-21 22:03:16 -0400

    add console logs

--- js/scripts.js
+++ js/scripts.js
@@ -9,29 +9,28 @@ $(function () {
     ).done(function () {
         let year = new Date();
         
-        let pageLoc = window.location.pathname;
-        let navLink = pageLoc.split("/");
-        let pagePop = navLink.pop();
-        let navPop = navLink.pop();
+        let pageLoc = window.location.pathname;         console.log("pageLoc " + pageLoc);
+        let navLink = pageLoc.split("/");               console.log("navLink " + navLink);
+        let pagePop = navLink.pop();                    console.log("pagePop " + pagePop);
+        let navPop = navLink.pop();                     console.log("navPop " + navPop);
 
-        let pageLink = ('a[href$="' + pageLoc + '"]');
-        let pageNav = ('[class$="' + navPop + '"]');
-        let breadcrumbPage = '';
-        let breadcrumbNav = navPop.substr(0, 1).toUpperCase() + navPop.substr(1);
+        let pageLink = ('a[href$="' + pageLoc + '"]');  console.log("pageLink " + pageLink);
+        let pageNav = ('[class$="' + navPop + '"]');    console.log("pageNav " + pageNav);
+        let breadcrumbPage = ''; 
+        let breadcrumbNav = navPop.substr(0, 1).toUpperCase() + navPop.substr(1); 
+                                                        console.log("breadcrumbNav " + breadcrumbNav);
 
         $("#year").text(year.getFullYear());
 
         if ($(pageLink).hasClass("dropdown-item")) {
             $(pageLink).addClass("active bg-menu-active");
-            console.log($(pageLink).text());
             breadcrumbPage = $(pageLink).text();        
             $("#page-title").html('<h3>' + breadcrumbPage + '</h3>');    
         } else {
             $("#page-title").html('<h3>' + breadcrumbNav + '</h3>'); 
         }
-        
+
         if ($("ol").has("#breadcrumb")) {
-            console.log(pageLoc);
             if (!(pagePop.trim()) || (pagePop.indexOf("index") >= 0)) {
                 console.log("parent test");
                 $("#breadcrumb").html('<li class="breadcrumb-item"><a href="/brown/index.html">Home</a></li><li class="breadcrumb-item active">' + breadcrumbNav + '</li>');
@@ -43,7 +42,6 @@ $(function () {
 
         $(pageLink).attr('href', '#');
         $(pageNav).addClass("active");    
-
 
         $(window).scroll(function () {
             if ($(this).scrollTop()) {
